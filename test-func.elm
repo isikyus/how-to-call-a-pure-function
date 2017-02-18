83497f7513a91111221a037c28309eafc1df162c
Isikyus <edward@palarang.com>
2017-02-18 12:08:35 +1100

    Second attempt at Elm test function

--- test-func.elm
+++ test-func.elm
@@ -1,4 +1,38 @@
-module Test exposing (..)
+port module Test exposing (..)
 
-test : String -> String
-test string = string ++ "a"
+-- Elm module to get a sense of how to call Elm from JS
+-- Very roughly based on https://gist.github.com/evancz/e69723b23958e69b63d5b5502b0edf90
+
+import String
+
+init : ( Model, Cmd Msg )
+init = ( Model "", Cmd.none )
+
+-- MODEL
+
+type alias Model = { text : String }
+
+-- UPDATE
+
+type Msg
+  = Done
+  | Test (String)
+
+port done : String -> Cmd msg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Test newText ->
+      ( Model newText, Cmd.none )
+
+    Done ->
+      ( model, done (model.text ++ "a") )
+
+-- SUBSCRIPTIONS
+
+port test : (String -> msg) -> Sub msg
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  test Test
